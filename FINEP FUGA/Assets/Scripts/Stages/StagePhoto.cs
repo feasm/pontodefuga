1993273eb05082990eb1c66eedb6a504c05025a2
Felipe Melo <feasm159@gmail.com>
2018-03-18 22:15:22 -0300

    Merge Andrei

---
+++ FINEP FUGA/Assets/Scripts/Stages/StagePhoto.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StagePhoto : MonoBehaviour {
+
+	public Camera camera;
+
+	void Start () {
+		Setup ();
+	}
+
+	void Update () {
+		
+	}
+
+	private void Setup() {
+		SetWidthToCamera ();
+	}
+
+	private void SetWidthToCamera() {
+		var width = Camera.main.orthographicSize * 2.0 * Screen.width / Screen.height;
+
+		var spriteRenderer = GetComponent<SpriteRenderer> ();
+		Sprite s = spriteRenderer.sprite;
+		float unitWidth = s.textureRect.width / s.pixelsPerUnit;
+
+		transform.localScale = new Vector3((float)width / unitWidth * 4, (float)width / unitWidth * 4);
+	}
+}
