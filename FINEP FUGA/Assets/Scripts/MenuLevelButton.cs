3c7cde599fe561bb5035cf340d280bc84dd12d5e
AndreiRafael <abrongel@gmail.com>
2018-03-18 21:08:29 -0300

    menu 2.0
    -menu com slide horizontal na parte de fora

---
+++ FINEP FUGA/Assets/Scripts/MenuLevelButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuLevelButton : MonoBehaviour {
+    public Image[] starImages;
+    public Color starInactiveColor = Color.gray, starActiveColor = Color.white;
+    Image selfImage;
+    public Color selfInactiveColor = Color.gray, selfActiveColor = Color.white;
+    public string levelKey = "";
+
+	// Use this for initialization
+	void Start () {
+        Setup();
+	}
+
+    void Setup() {
+        //TODO; Chamar ShowStarts com o número de estrelas ganhas no nível
+        ShowStarts(2);
+    }
+
+    void ActivateSelf(bool value = true) {
+        selfImage.color = value ? selfActiveColor : selfInactiveColor;
+    }
+
+    void ShowStarts(int amount) {
+        for (int i = 0; i < starImages.Length; i++)
+            starImages[i].color = i < amount ? starActiveColor : starInactiveColor;
+    }
+}
--- FINEP FUGA/Assets/Scripts/MenuSlowerScroll.cs
+++ FINEP FUGA/Assets/Scripts/MenuSlowerScroll.cs
@@ -8,19 +8,23 @@ public class MenuSlowerScroll : MonoBehaviour {
     RectTransform thisRect;
     public float intensity = 0.1f;
     Vector2 startPosition;
+    Vector2 contentStartPosition;
 	// Use this for initialization
 	void Start () {
         thisRect = GetComponent<RectTransform>();
         if(thisRect != null)
             startPosition = thisRect.anchoredPosition;
+        if (scrollRect != null)
+            contentStartPosition = scrollRect.content.anchoredPosition;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
         if (thisRect == null) return;
 
-        var calculatedPos = startPosition;
+        var calculatedPos = startPosition;// -contentStartPosition;
         calculatedPos.y += scrollRect.content.anchoredPosition.y * intensity;
+        calculatedPos.x += scrollRect.content.anchoredPosition.x * intensity;
         thisRect.anchoredPosition = calculatedPos;
 	}
 }
