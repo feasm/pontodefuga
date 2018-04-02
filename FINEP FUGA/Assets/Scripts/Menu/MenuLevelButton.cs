229e3e50253242ddc54a5aef99eef6f2e5904a9e
AndreiRafael <abrongel@gmail.com>
2018-04-02 15:16:33 -0300

    menu 3.0
    title antes de mostrar as fases
    transições +smooths
    ajuda em texto além da ajuda em áudio
    singleton pra conversação menu-jogo

---
+++ FINEP FUGA/Assets/Scripts/Menu/MenuLevelButton.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuLevelButton : MonoBehaviour {
+    public GameObject[] activeStars;
+    public GameObject[] inactiveStars;
+    public Image title;
+    public GameObject activePanel;
+    public string levelKeyPrefix = "stars_world_00_stage_00";
+    public int level = 1;
+    public string inactiveMessage;
+    public MenuNotification notificationManager;
+    public MenuFlowManager flowManager;
+
+	// Use this for initialization
+	void Start () {
+        Setup();
+	}
+
+    void Setup() {
+        //TODO; Chamar ShowStarts com o número de estrelas ganhas no nível
+        ShowStarts(PlayerPrefs.GetInt(levelKeyPrefix + level.ToString("00"), 0));
+        ActivateSelf(level <= 3);
+    }
+
+    void ActivateSelf(bool value = true) {
+        Color c = Color.white;
+        c.a = value ? 1.0f : 0.5f;
+        if(title != null)
+            title.color = c;
+
+        if(activePanel != null)
+            activePanel.SetActive(value);
+    }
+
+    void ShowStarts(int amount) {
+        for (int i = 0; i < activeStars.Length; i++) {
+            activeStars[i].SetActive(i < amount);
+        }
+        for (int i = 0; i < inactiveStars.Length; i++) {
+            inactiveStars[i].SetActive(i >= amount);
+        }
+    }
+
+    public void Click() {
+        if (activePanel != null && !activePanel.activeSelf)
+            notificationManager.ShowMessage(inactiveMessage);
+    }
+
+    public void SelectWorld(){
+        if (activePanel == null || activePanel.activeSelf)
+            flowManager.SelectWorld(level);
+    }
+
+    public void SelectLevel() {
+        if (activePanel == null || activePanel.activeSelf)
+            flowManager.SelectLevel(level);
+    }
+
+    public void FadeToPanel(int id) {
+        if (activePanel == null || activePanel.activeSelf)
+            flowManager.FadeAndChangePanel(id);
+    }
+}
---
+++ FINEP FUGA/Assets/Scripts/StageInfo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageInfo : MonoBehaviour {
+    public static StageInfo instance = null;
+
+    float stageTime = 0f;
+    float difficultyMultiplier = 1f;
+    public float[] starTimes;//tempos para ganhar cada quantia de estrelas
+    int worldID = 0, stageID = 0;
+	
+	void Awake () {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+	}
+
+    public void SetStageInfo(float time) {
+        stageTime = time;
+    }
+
+    public void SetDifficultyMultiplier(float multiplier) {
+        difficultyMultiplier = multiplier;
+    }
+
+    public void SetWorldID(int id) {
+        worldID = id;
+    }
+
+    public int GetWorldID() {
+        return worldID;
+    }
+
+    public void SetStageID(int id) {
+        stageID = id;
+    }
+
+    public int GetStageID() {
+        return stageID;
+    }
+}
