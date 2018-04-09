42fd07247b828d42a22fe042e34fa7ea1bb4c3e0
AndreiRafael <abrongel@gmail.com>
2018-04-09 14:37:49 -0300

    finalização do fluxo gameplay-menu

---
+++ FINEP FUGA/Assets/Scripts/GameUI/GameplayTransitions.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameplayTransitions : MonoBehaviour {
+    public RectTransform[] fadeRects;
+    public RectTransform canvas;
+
+    void Start() {
+        for (int i = 0; i < fadeRects.Length; i++)
+            StartCoroutine(IFadeRect(fadeRects[i], 0f, 2f));
+    }
+
+    public void FadeAndLoadScene(string sceneName) {
+        StartCoroutine(IFadeAndLoadScene(sceneName));
+    }
+
+    public void FadeAndReloadScene() {
+        FadeAndLoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    IEnumerator IFadeAndLoadScene(string sceneName) {
+        for (int i = 0; i < fadeRects.Length; i++)
+            StartCoroutine(IFadeRect(fadeRects[i], canvas.sizeDelta.y / 2f, 2f));
+
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator IFadeRect(RectTransform rect, float height, float speed = 2f) {
+        float lerp = 0f;
+        while (lerp < 1f) {
+            lerp += Time.deltaTime * speed;
+
+            Vector2 sizeDelta = rect.sizeDelta;
+            sizeDelta.y = Mathf.Lerp(sizeDelta.y, height, lerp);
+            rect.sizeDelta = sizeDelta;
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ FINEP FUGA/Assets/Scripts/GameUI/StepChecker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepChecker : MonoBehaviour {
+    static StepChecker manager;
+    public RectTransform[] checkRects;
+
+    void Awake() {
+        manager = this;
+    }
+
+    public static void CheckMarkAtIndex(int index) {
+        manager.CheckMark(index);
+    }
+
+    public void CheckMark(int index) {
+        if (index < 0 || index >= checkRects.Length) return;
+
+        checkRects[index].gameObject.SetActive(true);
+        StartCoroutine(IPopMark(index));
+    }
+
+    IEnumerator IPopMark(int index, float normal = 1f, float pop = 2f, float speed = 2.5f) {
+        if (index >= 0 && index < checkRects.Length) {
+            var rect = checkRects[index];
+
+            float lerp = 0f;
+            while (lerp < 1f) {
+                lerp += Time.deltaTime * speed;
+
+                rect.localScale = Vector3.Lerp(rect.localScale, Vector3.one * (lerp < 0.5f ? pop : normal), lerp);
+
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+}
